d6dd4998a65602811ac41c9be28a1a1d1a1a3a5d
husnilk <husnil.k@gmail.com>
2017-07-06 17:32:12 +0700

    Update AppPreferences.java

--- app/src/main/java/net/husnilkamil/popularmoview/data/AppPreferences.java
+++ app/src/main/java/net/husnilkamil/popularmoview/data/AppPreferences.java
@@ -2,6 +2,6 @@ package net.husnilkamil.popularmoview.data;
 
 public class AppPreferences {
     
-    public static final String API_KEY = "cf51c94af17e64e7a0b2fdf107a3dbc6";
+    public static final String API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
 }
